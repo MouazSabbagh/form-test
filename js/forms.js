39209d2e73d8689331e994a44ec0f9cb153e21a6
Achill <Achille@yahoo.com>
2018-09-13 05:02:33 -0700

    forms

--- js/forms.js
+++ js/forms.js
@@ -1,14 +1,20 @@
-console.log("hello");
+
 var form = document.querySelector("form");
-var thing = document.querySelector(".thing");
-console.log(form, thing);
-form.addEventListener("input", function(e){
-if (e.target.name === "amount"){
-  const value = parseFloat(e.target.value);
 
-  console.log(value * 100,  value * 100 + "%" );
-  thing.style.top = value * 100 + "%" ;
-
+form.addEventListener("input", function(e) {
+  console.log(e.type, e.target, e);
   
-}
+});
+form.addEventListener("change", function(e) {
+  console.log(e.type, e.target, e);
+  
+});
+form.addEventListener("focusout", function(e) {
+  console.log(e.type, e.target, e);
+  
+});
+form.addEventListener("submit", function(e) {
+  console.log(e.type, e.target, e);
+  e.preventDefault();
+  
 });
